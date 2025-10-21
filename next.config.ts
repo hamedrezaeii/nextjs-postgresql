dae3eaf143a6dc6c00bcc6b1e4c3ff5faa488ded
hamed rezaei <hamedrezaei49@gmail.com>
2025-10-21 19:51:27 +0330

    update next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    serverActions: true,
+  },
+  output: "standalone", // مهم برای Serverless با DB
 };
 
-export default nextConfig;
+module.exports = nextConfig;
